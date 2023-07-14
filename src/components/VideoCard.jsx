63cc972aed4e35a1bec61c4e57df6e9b40732253
Branden Ng <brandendanielng@hotmail.com>
2023-07-15 01:07:49 +1000

    progress

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -19,16 +19,16 @@ const VideoCard = ({
   return (
     <Card
       sx={{
-        width: { md: "320px", xs: "100%" },
+        width: { xs: "100%", md: "320px" },
         boxShadow: "none",
-        borderRadius: 0,
+        borderRadius: "none",
       }}
     >
       <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
         <CardMedia
           image={snippet?.thumbnails?.high?.url}
           alt={snippet?.title}
-          sx={{ width: 358, height: 180 }}
+          sx={{ width: "100%", height: 180 }}
         />
       </Link>
       <CardContent sx={{ backgroundColor: "#1e1e1e", height: "106px" }}>
