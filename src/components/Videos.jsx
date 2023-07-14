56021338296f4bb68f50c1355a6559527a17bb1a
Branden Ng <brandendanielng@hotmail.com>
2023-07-14 23:11:38 +1000

    progress

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { Stack, Box } from "@mui/material";
+import { VideoCard } from "./";
+import { ChannelCard } from "./";
 
 const Videos = ({ videos }) => {
   console.log(videos);
   return (
     <Stack direction="row" flexWrap="wrap" justifyContent="flex-start" gap={2}>
       {videos.map((item, idx) => (
-        <Box key={idx}></Box>
+        <Box key={idx}>
+          {item.id?.videoId && <VideoCard video={item} />}
+          {/* {item.id?.channelId && <ChannelCard channelDetail={item} />} */}
+        </Box>
       ))}
     </Stack>
   );
